8938c6bf1e19989aa994a24b444540275c99afed
himanshunete <himanshuneteh@gmail.com>
2021-02-03 07:44:10 +0530

    [himanshunete]Merge CompareLengthOfTwoLines

--- LineComparisonComputation/Program.cs
+++ LineComparisonComputation/Program.cs
@@ -6,7 +6,37 @@ namespace LineComparisonComputation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" Welcome to Line Computation Program ");            
+            Console.WriteLine(" Welcome to Line Computation Program ");
+            //Coordinates of Line1
+            double x1x = 4;
+            double y1x = 6;
+            double x2x = 8;
+            double y2x = 7;
+
+            //Coordinates of Line2
+            double x1y = 4;
+            double y1y = 6;
+            double x2y = 8;
+            double y2y = 7;
+            double squareXx = (x2x - x1x) * (x2x - x1x);
+            double squareYx = (y2x - y1x) * (y2x - y1x);
+            double squareXy = (x2y - x1y) * (x2y - x1y);
+            double squareYy = (y2y - y1y) * (y2y - y1y);
+            double lengthOfLine1 = Math.Sqrt(squareXx + squareYx);
+            double lengthOfLine2 = Math.Sqrt(squareXy + squareYy);
+            if (lengthOfLine1 == lengthOfLine2)
+            {
+                Console.WriteLine("Length of Two Lines are Equal");
+            }
+            else if (lengthOfLine1 > lengthOfLine2)
+            {
+                Console.WriteLine("Length of Line1 is Greater than Line 2 ");
+            }
+
+            else if (lengthOfLine1 < lengthOfLine2)
+            {
+                Console.WriteLine("Length of Line1 is lesser than Line 2 ");
+            }
         } 
     }
 }
